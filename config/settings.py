547f3310a4b79e8c1a9abeb8f50fc9d0559a7f70
OldSnakeEater <mihail.hudopay@mail.ru>
2026-06-02 21:32:13 +0300

    chore: add CORS

--- config/settings.py
+++ config/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'currency_exchange'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://0.0.0.0:80",
+]
 
 TEMPLATES = [
     {
